034327bb599dad681d7106653433da6ef6129f3b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-04-06 19:44:39 +0200

    Make hitsounds functional
    Port number needs to be manually configured in the script for hitsounds
    to play from the correct player's perspective.

--- node_2d.gd
+++ node_2d.gd
@@ -1,12 +1,12 @@
-extends Node2D
+extends Control
 
 # Network protocol used by Slippi:
-# https:#github.com/jordan-zilch/slippi-wiki/blob/master/SPECTATOR_PROTOCOL.md
+# https://github.com/jordan-zilch/slippi-wiki/blob/master/SPECTATOR_PROTOCOL.md
 
 # Binary payload format:
-# https:#github.com/project-slippi/slippi-wiki/blob/master/SPEC.md
+# https://github.com/project-slippi/slippi-wiki/blob/master/SPEC.md
 
-# https:#github.com/project-slippi/slippi-js/blob/efbafa721e272283a7924975f1dc8295ac522dac/src/console/types.ts#L19-L23
+# https://github.com/project-slippi/slippi-js/blob/efbafa721e272283a7924975f1dc8295ac522dac/src/console/types.ts#L19-L23
 const SLIPPI_DEFAULT_PORT = 51441
 
 var enet_connection := ENetConnection.new()
@@ -14,6 +14,20 @@ var enet_packet_peer: ENetPacketPeer
 var handshake_sent := false
 
 var event_payloads := PackedByteArray()
+
+# Damage percentages of each player.
+var player1_percentage := 0.0
+var player2_percentage := 0.0
+var player3_percentage := 0.0  # TODO: Support doubles (2v2)
+var player4_percentage := 0.0  # TODO: Support doubles (2v2)
+
+# Which controller port you're on (determines which hitsounds are played).
+# TODO: Determine automatically based on player nametags. From my testing,
+# this doesn't seem to be feasible for replays at least, as Game Start
+# is never received by the client.
+var port := 1
+
+@onready var audio_stream_player := $AudioStreamPlayer as AudioStreamPlayer
 
 func _ready() -> void:
 	# Only 1 peer and 1 channel is needed (the running Dolphin Slippi instance).
@@ -67,39 +81,31 @@ func _physics_process(_delta: float) -> void:
 							0x39:
 								print("Game End")
 							0x3a:
-								# Find Item Updates in between.
-								var post_frame_idx := 0
 								const FRAME_START_SIZE = 12 + 1
-								var pl = payload.slice(FRAME_START_SIZE + 0x3c, FRAME_START_SIZE + 0x3c + 5)
-								#pl.reverse()
-								print(pl.decode_float(0))
-								const PRE_FRAME_SIZE = 64 + 1
-								const ITEM_SIZE = 44 + 1
-								var item_idx := payload.find(0x3b, FRAME_START_SIZE + PRE_FRAME_SIZE + PRE_FRAME_SIZE)
-								if item_idx > 0:
-									# Found an item find other items.
-									while item_idx > 0:
-										#print("item")
-										item_idx = payload.find(0x3b, item_idx + ITEM_SIZE)
+								const PERCENT_OFFSET = 0x3c
+								var pl1 = payload.slice(FRAME_START_SIZE + PERCENT_OFFSET, FRAME_START_SIZE + PERCENT_OFFSET + 4)
+								pl1.reverse()
+								# Empirically determined by looking at the binary stream.
+								const OFFSET_BETWEEN_PLAYERS = 0x41
+								var pl2 = payload.slice(FRAME_START_SIZE + PERCENT_OFFSET + OFFSET_BETWEEN_PLAYERS, FRAME_START_SIZE + PERCENT_OFFSET + OFFSET_BETWEEN_PLAYERS + 4)
+								pl2.reverse()
+								var new_player1_percentage := pl1.decode_float(0)
+								var new_player2_percentage := pl2.decode_float(0)
 
-									post_frame_idx = item_idx + ITEM_SIZE
-								else:
-									#print("no item")
-									post_frame_idx = FRAME_START_SIZE + PRE_FRAME_SIZE + PRE_FRAME_SIZE
+								if new_player1_percentage > player1_percentage:
+									var damage_dealt := new_player1_percentage - player1_percentage
+									if port == 2:
+										audio_stream_player.pitch_scale = remap(damage_dealt, 0.0, 100.0, 1.0, 2.0)
+										audio_stream_player.play()
 
+								if new_player2_percentage > player2_percentage:
+									var damage_dealt := new_player1_percentage - player1_percentage
+									if port == 1:
+										audio_stream_player.pitch_scale = remap(damage_dealt, 0.0, 100.0, 1.0, 2.0)
+										audio_stream_player.play()
 
-								print(payload.hex_encode())
-								#printt(post_frame_idx, payload[post_frame_idx])
-								#print()
-
-								var percent := payload.slice(post_frame_idx + 0x16, post_frame_idx + 0x16 + 5)
-								# Data is big-endian, so reverse it before decoding it.
-								percent.reverse()
-								#printt("Percent P1:", percent.decode_float(0))
-								#var idx2 := payload.find(0x38)
-								#if idx2 > 0:
-									#printt("Percent P2:", payload.decode_float(idx2 + 0x16))
-
+								player1_percentage = new_player1_percentage
+								player2_percentage = new_player2_percentage
 							0x3b:
 								print("Item Update")
 							0x3c:
@@ -109,74 +115,7 @@ func _physics_process(_delta: float) -> void:
 							0x10:
 								print("Message Splitter")
 
-						if false and payload[0] == 0x38:
-							printt("Post-frame percent:", payload.decode_float(0x16))
-
 
 func _exit_tree() -> void:
 	print("Disconnecting...")
 	enet_packet_peer.peer_disconnect()
-
-
-# https://github.com/project-slippi/slippi-js/blob/efbafa721e272283a7924975f1dc8295ac522dac/src/utils/slpReader.ts#L250-L324
-#func parse_payload(payload: PackedByteArray) -> int:
-	#var readPosition = startPos != null && startPos > 0 ? startPos : slpFile.rawDataPosition
-	#var stopReadingAt = slpFile.rawDataPosition + slpFile.rawDataLength
-#
-	## Generate read buffers for each
-	#var commandPayloadBuffers = mapValues(slpFile.messageSizes, (size) => new Uint8Array(size + 1))
-	#var splitMessageBuffer = new Uint8Array(0)
-#
-	#var commandByteBuffer = new Uint8Array(1)
-	#while readPosition < stopReadingAt:
-		#readRef(ref, commandByteBuffer, 0, 1, readPosition)
-		#var commandByte = (commandByteBuffer[0] as number) ?? 0
-		#var buffer = commandPayloadBuffers[commandByte]
-		#if buffer == null:
-			## If we don't have an entry for this command, return false to indicate failed read.
-			#return readPosition
-#
-#
-		#if buffer.length > stopReadingAt - readPosition:
-			#return readPosition
-#
-		#var advanceAmount = buffer.length
-#
-		#readRef(ref, buffer, 0, buffer.length, readPosition)
-		#if commandByte === Command.SPLIT_MESSAGE:
-		  ## Here we have a split message, we will collect data from them until the last
-		  ## message of the list is received.
-		  #var view = new DataView(buffer.buffer)
-		  #var size = readUint16(view, 0x201) ?? 512
-		  #var isLastMessage = readBool(view, 0x204)
-		  #var internalCommand = readUint8(view, 0x203) ?? 0
-#
-		  ## If this is the first message, initialize the splitMessageBuffer
-		  ## with the internal command byte because our parseMessage function
-		  ## seems to expect a command byte at the start.
-		  #if splitMessageBuffer.length === 0:
-			#splitMessageBuffer = new Uint8Array(1)
-			#splitMessageBuffer[0] = internalCommand
-#
-		  ## Collect new data into splitMessageBuffer.
-		  #var appendBuf = buffer.slice(0x1, 0x1 + size)
-		  #var mergedBuf = new Uint8Array(splitMessageBuffer.length + appendBuf.length)
-		  #mergedBuf.set(splitMessageBuffer)
-		  #mergedBuf.set(appendBuf, splitMessageBuffer.length)
-		  #splitMessageBuffer = mergedBuf
-#
-		#if isLastMessage:
-			#commandByte = splitMessageBuffer[0] ?? 0
-			#buffer = splitMessageBuffer
-			#splitMessageBuffer = new Uint8Array(0)
-#
-#
-		#var parsedPayload = parseMessage(commandByte, buffer)
-		#var shouldStop = callback(commandByte, parsedPayload, buffer)
-		#if shouldStop:
-		  #break
-#
-		#readPosition += advanceAmount
-	  #}
-#
-	  #return readPosition
